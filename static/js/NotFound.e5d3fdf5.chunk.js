ccde5a51d2d53436fdde33f2489a18130828ae12
xaletch <kiril.kolesnikov0609@mail.ru>
2023-04-24 18:51:21 +0300

    Updates

---
+++ static/js/NotFound.e5d3fdf5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_shop=self.webpackChunkreact_shop||[]).push([[187],{4949:function(e,r,n){n.r(r);n(2791);var s=n(1969),t=n(184);r.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(s.vr,{})})}}}]);
+//# sourceMappingURL=NotFound.e5d3fdf5.chunk.js.map
