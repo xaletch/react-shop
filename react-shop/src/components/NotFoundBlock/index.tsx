1ce438324fc0f4f6f8919d14d1b285d22a02fef3
xaletch <kiril.kolesnikov0609@mail.ru>
2023-05-06 18:56:09 +0300

    add

--- react-shop/src/components/NotFoundBlock/index.tsx
+++ react-shop/src/components/NotFoundBlock/index.tsx
@@ -6,7 +6,7 @@ export const NotFoundBlock: React.FC = () => {
   return (
     <div className={styles.main}>
         <h1 className={styles.h1}>Ничего не найдено :(</h1>
-        <p className={styles.description}>К сожалению данная странеца не найдена</p>
+        <p className={styles.description}>К сожалению данная странеца не найдена. попробуйте обновить страницу</p>
     </div>
   )
 }
